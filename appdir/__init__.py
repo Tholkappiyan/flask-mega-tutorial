68a545c28a406d4e8c833937b4f29432dbd7ed4c
Tholkappiyan <vtholkappiyan@gmail.com>
2018-12-20 16:06:52 +0530

    Chapter 10 - Email & Threading
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-x-email-support

--- appdir/__init__.py
+++ appdir/__init__.py
@@ -1,11 +1,12 @@
+import os
+import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import os
-import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['DIRECT_SECRET_KEY'] = 'you-will-never-guess'
@@ -21,6 +22,8 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+mail = Mail(app)
 
 from appdir import routes, models, errors
 

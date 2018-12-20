80dba21652811e013408215ad509b9902a9cbcf1
Tholkappiyan <vtholkappiyan@gmail.com>
2018-12-20 17:22:11 +0530

    Chapter 13 : i19n & l10n partial
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xiii-i18n-and-l10n

--- appdir/__init__.py
+++ appdir/__init__.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +9,8 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask_babel import lazy_gettext as _l
 
 app = Flask(__name__)
 app.config['DIRECT_SECRET_KEY'] = 'you-will-never-guess'
@@ -24,11 +26,13 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from appdir import routes, models, errors
 
@@ -59,3 +63,8 @@ if not app.debug:
 
     app.logger.setLevel(logging.DEBUG)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    # return 'es'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
--- appdir/routes.py
+++ appdir/routes.py
@@ -6,6 +6,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from appdir.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
+from flask_babel import _
 
 @app.before_request
 def before_request():
@@ -22,7 +23,7 @@ def index():
         post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        flash('Your post is now live!')
+        flash(_('Your post is now live!'))
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
@@ -106,7 +107,7 @@ def edit_profile():
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
+        flash(_('User %(username)s not found.', username=username))
         return redirect(url_for('index'))
     if user == current_user:
         flash('You cannot follow yourself!')
